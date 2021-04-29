18340b4eb7cbbf5e03d68dfce017f5f04e1a5e31
Patrick <pdemers026@gmail.com>
2021-04-29 16:10:23 -0700

    Hello, World

--- Meme Project.py
+++ Meme Project.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import flask_sqlalchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQALchemy(app)
+
+class Toda(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    data_created= db.
 @app.route('/')
 def hello():
     return 'Hello, World!'
